0b42fbbb1470cb708bb7e0fb2814a3484c6ee3f4
Awat Singdamrong <a.lubing@yahoo.com>
2024-03-28 22:05:51 +0700

    feat: Login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { toast } from 'react-toastify'
 import { FaSignInAlt } from 'react-icons/fa'
+import { useSelector, useDispatch } from 'react-redux'
+import { login, reset } from '../features/auth/authSlice'
+import { useNavigate } from 'react-router-dom'
 function Login() {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     });
     const { name, email, password, password2 } = formData;
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { user, isLoading, isError, isSuccess,
+        message } = useSelector(state => state.auth)
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+        //redirect when logged in
+        if (isSuccess || user) {
+            navigate('/')
+        }
+        dispatch(reset())
+    }, [isError, isSuccess, user, message, navigate, dispatch])
     const onChange = (e) => {
         setFormData((prevState) => ({
             ...prevState,
@@ -15,6 +32,11 @@ function Login() {
     }
     const onSubmit = (e) => {
         e.preventDefault()
+        const userData = {
+            email,
+            password
+        }
+        dispatch(login(userData))
     }
     return (
         <>
@@ -27,12 +49,13 @@ function Login() {
             <section className='form'>
                 <form onSubmit={onSubmit}>
                     <div className="form-group">
-                        <input type="email" className='form-control' id='email' name='email'
-                            value={email} onChange={onChange} placeholder='Enter Your email' required />
+                        <input type="email" className='form-control'
+                            id='email' name='email' value={email} onChange={onChange}
+                            placeholder='Enter Your email' required />
                     </div>
                     <div className="form-group">
-                        <input type="password" className='form-control' id='password' name='password'
-                            value={password} onChange={onChange} placeholder='Enter Your password' required />
+                        <input type="password" className='form-control' id='password' name='password' value={password}
+                            onChange={onChange} placeholder='Enter Your password' required />
                     </div>
                     <div className='form-group'>
                         <button className='btn btn-block'>Submit</button>
